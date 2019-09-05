107a50afdcab9d0f77a051e18d9c479e3ba1bb7b
aaronamm <theeranitp@gmail.com>
2019-09-06 00:01:39 +0700

    Click to change opacity value

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div> hello</div>;
+  const [currentOpacity, setCurrentOpacity] = useState(0);
+  const defaultOpacity = 0;
+  const maxOpacity = 1;
+  const numberOfClickTimes = 10;
+  const opacityIncrement = maxOpacity / numberOfClickTimes;
+
+  const style = {
+    backgroundColor: `rgba(204,204,204, ${defaultOpacity})`,
+    width: "200px",
+    height: "200px",
+    border: "1px solid #000"
+  };
+
+  const handleClick = e => {
+    let newOpacity = currentOpacity + opacityIncrement;
+    e.target.style.backgroundColor = `rgba(204,204,204, ${newOpacity})`;
+    setCurrentOpacity(newOpacity);
+  };
+
+  return <div style={style} onClick={handleClick}></div>;
 }
 
 export default App;
